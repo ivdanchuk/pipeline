8b4f2818598fb34220031dfc657c74297088f46f
Igor Danchuk <ivdanchuk@gmail.com>
2022-01-28 22:11:04 +0200

    first commit to dev

---
+++ src/main/java/com/example/demo/StaticMethod.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class StaticMethod {
+    public static String Hello (){
+        return "hi !";
+    }
+}
---
+++ src/test/java/com/example/demo/StaticMethodTest.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(fullyQualifiedNames = "com.example.demo.StaticMethod")
+
+public class StaticMethodTest {
+    @Test
+    public void Test1 (){
+        mockStatic(StaticMethod.class);
+        when(StaticMethod.Hello()).thenReturn("Hello Baeldung!");
+        String firstWelcome = StaticMethod.Hello();
+        assertEquals("Hello Baeldung!", firstWelcome);
+    }
+}
